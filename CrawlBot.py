3985f36ce703eab02565b177420913003dc40ecb
hauntps <hauntps@gmail.com>
2019-06-03 17:30:14 +0700

    Stackoverflow Bot v1.1

---
+++ CrawlBot.py
@@ -0,0 +1,60 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+import numpy as np
+from console_logging.console import Console
+import json
+import os, re
+
+console = Console()
+curated_lists = []
+browser = webdriver.Chrome()
+console.info("Initialized Chrome Webdriver.")
+
+def get_repos(pages=100):
+    console.log("Now entering signup process.")
+    # Get repos
+    console.info("Loaded search results.")
+    for page in range(pages):
+        browser.get(
+        'https://stackoverflow.com/tags?page=%d&tab=popular' %page)
+        search_results = browser.find_elements_by_class_name('grid-layout--cell')
+        console.info("Found %d results..." % len(search_results))
+        sleep(1)
+        for search_result in search_results:
+            try:
+                # Get text content in repo-list-item class by innerText JS attribute
+                resultText = search_result.get_attribute('innerText').split('\n')
+                tagName = repr(resultText[0].encode('ascii', 'ignore'))
+                tagNameList =  list(tagName)
+                tagNameList =  tagNameList[1 : len(tagNameList) - 1]
+                tagName = ''.join(str(tag) for tag in tagNameList)
+                tagNameList = re.findall("\D|[\D\d]\D", tagName)
+                tagName = ''.join(str(tag) for tag in tagNameList)
+                #tagName = ''.join([_str for _str in tagName if not _str.isdigit()]).title()
+
+                descriptionTag = repr(resultText[1].encode('ascii', 'ignore')).title()
+                descriptionTagList =  list(descriptionTag)
+                descriptionTagList =  descriptionTagList[1 : len(descriptionTagList) - 1]
+                descriptionTag = ''.join(str(description) for description in descriptionTagList)
+                curated_lists.append({
+                    'tagName': tagName,
+                    'descriptionTag': descriptionTag
+                })
+                console.success("Added " + tagName)
+            except Exception as e:
+                console.error(str(e))
+        sleep(2)
+        #browser.find_element_by_class_name("next").click()
+    #browser.quit()
+
+try:
+    get_repos(pages=1619)
+except:
+    pass
+
+with open('./TagList.json', 'w') as curated_list_json_file:
+    json.dump(curated_lists, curated_list_json_file, indent=4)
---
+++ json-to-md.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import os, json
+
+template='''# All Stackoverflow Tag List
+
+Ever wish there was a curated list for your curated lists, including other curated lists of curated lists that may or may not contain other curated lists?
+
+``` python
+print('A Stackoverflow Tag List ' + ('Stackoverflow Tag List of '*∞) + 'Stackoverflow Tag List.')
+```
+
+_Updated as often as I can. Want to contribute? Go ahead and make a pull request._
+
+- [x] Index top 1000 Stackoverflow Tag List.
+- [x] Make a bot do it for me
+- [x] Automate the curation entirely
+- [ ] Organize lists
+- [ ] Create easy search for lists.
+
+# Stackoverflow Tag List
+
+'''
+
+
+with open('README.md','w') as tag_list_md:
+    tag_list_md.write(template)
+    with open('TagList.json','r') as tag_list_json:
+        tag_lists = json.load(tag_list_json)
+        count = 1
+        for tag_list in tag_lists:
+            tag_list_md.write('__{}. [{}]({})__: {}\n\n'.format(
+                count,
+                tag_list['tagName'],
+                'https://stackoverflow.com/questions/tagged/' + tag_list['tagName'],
+                tag_list['descriptionTag']
+            ))
+            count = count + 1
